6563d42f300ddf773bdd0e05f7deeeb601611bc9
Xavier Belanche <xbelanch@protonmail.com>
2021-07-24 13:51:17 +0200

    Add basic node data structure

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node {
+    size_t data;
+    struct node *next;
+} Node;
+
+typedef struct list {
+    Node *firstNode;
+} List;
+
+Node *newNode(size_t data) {
+    Node *node = malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+List *list;
+
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv[0];
 
-    fprintf(stdout, "all!\n");
+    list = malloc(sizeof(List));
+    list->firstNode = newNode(0);
+    Node *node = list->firstNode;
+
+    while (NULL != node) {
+        fprintf(stdout, "data: %lu\n", node->data);
+        node = node->next;
+    }
 
     return 0;
 }
