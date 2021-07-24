663f9aae87489fd76c0af5435c2fe4aa250d4216
Xavier Belanche <xbelanch@protonmail.com>
2021-07-24 21:43:42 +0200

    Rewrite code based on Pat Morin's "Open Data Structures" site

--- main.c
+++ main.c
@@ -8,7 +8,17 @@ typedef struct node {
 
 typedef struct list {
     Node *head;
+    Node *tail;
+    size_t n;
 } List;
+
+List *newList() {
+    List *list = malloc(sizeof(List));
+    list->head = NULL;
+    list->tail = NULL;
+    list->n = 0;
+    return list;
+}
 
 Node *newNode(size_t data) {
     Node *node = malloc(sizeof(Node));
@@ -17,7 +27,29 @@ Node *newNode(size_t data) {
     return node;
 }
 
-Node *addNode(List *list, size_t data) {
+int push(List *list, size_t data) {
+    Node *node = newNode(data);
+    node->next = list->head;
+    list->head = node;
+    if (list->n == 0)
+        list->tail = node;
+    list->n++;
+    return 0;
+}
+
+int pop(List *list) {
+    if (list->n == 0)
+        return 0;
+
+    Node *remove = list->head;
+    list->head = list->head->next;
+    list->n--;
+    free(remove);
+
+    return 1;
+}
+
+Node *add(List *list, size_t data) {
     Node *node, *p;
     node = newNode(data);
     if (list->head == NULL) {
@@ -29,35 +61,13 @@ Node *addNode(List *list, size_t data) {
         }
         p->next = node;
     }
+    list->tail = node;
+    list->n++;
     return node;
 }
 
-Node *findNode(List *list, size_t data) {
-    if (NULL == list)
-        return NULL;
-    Node *p = list->head;
-    while (p->next != NULL) {
-        p = p->next;
-        if (p->data == data)
-            return p;
-    }
-
-    return NULL;
-}
-
-void insertAfter(Node *this, Node *that) {
-    that->next = this->next;
-    this->next = that;
-}
-
-// void insertAtBeginning(List *list, Node *this) {
-
-// }
-
-void removeAfter(Node *this) {
-    Node *remove = this->next;
-    this->next = this->next->next;
-    free(remove);
+int delete(List *list) {
+    return pop(list);
 }
 
 void listScan(List *list) {
@@ -68,37 +78,60 @@ void listScan(List *list) {
     }
 }
 
-void printNode(Node *node) {
-    if (NULL == node) {
-        fprintf(stderr, "Node with this data not found!\n");
-    } else {
-        fprintf(stdout, "data: %lu\n", node->data);
-    }
-}
 
-size_t listSize(List *list) {
-    size_t size = 0;
-    Node *node = list->head;
-    while (NULL != node->next)  {
-        size++;
-        node = node->next;
-    }
-    return size;
-}
+// Node *findNode(List *list, size_t data) {
+//     if (NULL == list)
+//         return NULL;
+//     Node *p = list->head;
+//     while (p->next != NULL) {
+//         p = p->next;
+//         if (p->data == data)
+//             return p;
+//     }
+
+//     return NULL;
+// }
+
+// void insertAfter(Node *this, Node *that) {
+//     that->next = this->next;
+//     this->next = that;
+// }
+
+// // void insertAtBeginning(List *list, Node *this) {
+
+// // }
+
+// void removeAfter(Node *this) {
+//     Node *remove = this->next;
+//     this->next = this->next->next;
+//     free(remove);
+// }
+
+// void printNode(Node *node) {
+//     if (NULL == node) {
+//         fprintf(stderr, "Node with this data not found!\n");
+//     } else {
+//         fprintf(stdout, "data: %lu\n", node->data);
+//     }
+// }
+
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv[0];
 
-    List *list = malloc(sizeof(List));
-    for (size_t i = 0; i < 16; ++i) {
-        addNode(list, i);
-    }
-    removeAfter(findNode(list, 8));
-    insertAfter(findNode(list, 10), newNode(32));
-    listScan(list);
-    fprintf(stdout, "List size: %lu\n", listSize(list));
+    List *list = newList();
 
+    for (size_t i = 0; i < 5; ++i)
+        add(list, i);
+
+    fprintf(stdout, "size: %lu\n", list->n);
+    pop(list);
+    fprintf(stdout, "size: %lu\n", list->n);
+    push(list, 12);
+    fprintf(stdout, "size: %lu\n", list->n);
+    add(list, 99);
+    listScan(list);
     return 0;
 }
