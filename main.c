f7ec07cb8f9d9e56864b4061ec0e751b2421916b
Xavier Belanche <xbelanch@protonmail.com>
2021-07-30 13:57:18 +0200

    wip linked list merge sort algorithm

--- main.c
+++ main.c
@@ -64,6 +64,7 @@ Node *add(List *list, size_t data) {
         p->next = node;
     }
     list->tail = node;
+    list->tail->next = NULL;
     list->n++;
     return node;
 }
@@ -87,6 +88,7 @@ Node *insert_after(List *list, Node *prev, size_t data) {
         } else {
             list->tail->next = node;
             list->tail = node;
+            list->tail->next = NULL;
         }
         list->n++;
         return node;
@@ -133,44 +135,6 @@ Node *search(List *list, size_t data) {
     return NULL;
 }
 
-// Sort a linked list using insertion sort
-// Robbed at the moment from https://www.learnc.net/c-data-structures/c-linked-list/
-// TODO: Fix list tail point to the right node
-Node* sort(List *list)
-{
-    Node *x, *y, *e;
-
-    x = list->head;
-    list->head = NULL;
-    list->tail = NULL;
-
-    while(x != NULL) {
-        e = x;
-        x = x->next;
-        if (list->head != NULL) {
-            if(e->data > list->head->data) {
-                y = list->head;
-                list->tail = e;
-                while ((y->next != NULL) && (e->data > y->next->data)) {
-                    y = y->next;
-                }
-                e->next = y->next;
-                y->next = e;
-            }
-            else {
-                e->next = list->head;
-                list->head = e ;
-            }
-        }
-        else {
-            e->next = NULL;
-            list->head = e;
-        }
-    }
-    return list->head;
-}
-
-
 void traverse(List *list, callback func)
 {
     Node* cursor = list->head;
@@ -185,7 +149,101 @@ void printNode(Node *node) {
         fprintf(stderr, "Node with this data not found!\n");
     } else {
         fprintf(stdout, "data: %lu\n", node->data);
+        if (node->next != NULL)  {
+            fprintf(stdout, "\tnext data: %lu\n", node->next->data);
+        } else {
+            fprintf(stdout, "\tnext data is NULL\n");
+        }
     }
+}
+
+Node *find_at_index(List *list, size_t index) {
+    if (index > list->n)
+        return NULL;
+
+    size_t n = 0;
+    Node *cursor = list->head;
+    while (cursor != NULL) {
+        if (n == index) {
+            return cursor;
+        }
+        cursor = cursor->next;
+        n++;
+    }
+
+    return NULL;
+}
+
+// Merge sort algorithm
+List *merge_sort(List *list) {
+    if (list->n <= 1)
+        return list;
+    printf("+++++++++++++++++++++\n");
+    printf("LEFT ----\n");
+    // left half list
+    Node *start = list->head;
+    Node *pointer = start;
+    Node *end= find_at_index(list, (list->n / 2));
+    List *leftList = newList();
+    while (pointer != end) {
+        add(leftList, pointer->data);
+        pointer = pointer->next;
+    }
+    // add(leftList, end->data);
+    traverse(leftList, printNode);
+    printf("RIGHT ----\n");
+
+    // right halft list
+    start = end;
+    pointer = start;
+    List *rightList = newList();
+    while (pointer != NULL) {
+        add(rightList, pointer->data);
+        pointer = pointer->next;
+    }
+    traverse(rightList, printNode);
+
+    merge_sort(leftList);
+    merge_sort(rightList);
+
+    return NULL;
+}
+
+
+// Sort a linked list using insertion sort
+// Robbed at the moment from https://www.learnc.net/c-data-structures/c-linked-list/
+// TODO: Fix list tail point to the right node
+Node* sort(List *list)
+{
+    Node *x, *y, *e;
+
+    x = list->head;
+    list->head = NULL;
+
+    while(x != NULL) {
+        e = x;
+        x = x->next;
+        if (list->head != NULL) {
+            if(e->data > list->head->data) {
+                y = list->head;
+                while ((y->next != NULL) && (e->data > y->next->data)) {
+                    y = y->next;
+                }
+                e->next = y->next;
+                y->next = e;
+            }
+            else {
+                e->next = list->head;
+                list->head = e;
+            }
+        }
+        else {
+            e->next = NULL;
+            list->head = e;
+        }
+    }
+
+    return list->head;
 }
 
 int main(int argc, char *argv[])
@@ -206,11 +264,17 @@ int main(int argc, char *argv[])
     insert_before(list, list->head, 101);
     insert_before(list, search(list, 330), 33);
 
-    sort(list);
+    // sort(list);
     traverse(list, printNode);
     fprintf(stdout, "Head value: %lu\n", list->head->data);
     fprintf(stdout, "Tail value: %lu\n", list->tail->data);
     fprintf(stdout, "Length of list: %lu\n", list->n);
+    fprintf(stdout, "Node value at half of list: %lu\n", find_at_index(list, list->n / 2)->data);
+
+
+    //
+    printf("+++ MERGE SORTING +++\n");
+    merge_sort(list);
     free(list);
     return 0;
 }
